1fe9d533767a42ba744c75c525163a4365b17ab0
RockyAtoyan <111198613+RockyAtoyan@users.noreply.github.com>
2026-02-03 14:18:27 +0300

    fix: hero entrance message adaptive

--- src/widgets/landing/hero.tsx
+++ src/widgets/landing/hero.tsx
@@ -22,10 +22,31 @@ export const Hero = () => {
       maskSize: initialMaskSize
     });
 
-    tl.to(".scale-out", {
-      scale: 1,
-      ease: "power1.inOut"
-    })
+    gsap.set(".scale-out", { scale: 1.25 });
+
+    const firstScreenTl = gsap.timeline();
+
+    firstScreenTl
+      .to(".hero-container", {
+        opacity: 1,
+        duration: 1,
+        ease: "sine.in(1.7)"
+      })
+      .fromTo(
+        ".scale-out",
+        {
+          scale: 1.5
+        },
+        {
+          scale: 1.25,
+          duration: 1,
+          ease: "sine.inOut(1.7)"
+        },
+        "<"
+      );
+
+    tl.set(".scale-out", { scale: 1.25 })
+      .to(".scale-out", { scale: 1, ease: "power1.inOut" })
       .to(
         ".fade-out",
         {
@@ -51,6 +72,7 @@ export const Hero = () => {
         },
         "<40%"
       )
+      .to(".mask-wrapper", { opacity: 0, ease: "sine.inOut(1.7)" }, "<40%")
       .to(
         ".entrance-message",
         {
@@ -58,9 +80,13 @@ export const Hero = () => {
             "radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)",
           visibility: "visible",
           duration: 0.6,
-          ease: "power1.inOut"
+          ease: "power1.inOut",
+          onComplete: () => {
+            console.log("hide mask-wrapper");
+            gsap.to(".mask-wrapper", { opacity: 0 });
+          }
         },
-        "<10%"
+        "<"
       )
       .to(
         ".entrance-info",
@@ -73,7 +99,7 @@ export const Hero = () => {
   });
 
   return (
-    <div>
+    <div className="hero-container opacity-0">
       <nav className="flex items-center justify-between fixed z-100 top-0 left-0 w-full md:py-12 md:px-16 p-5">
         <img
           className="size-8 md:size-[2.5dvw]"
@@ -90,12 +116,12 @@ export const Hero = () => {
         <div className="size-full bg-white mask-wrapper">
           <img
             src="/images/hero-bg.webp"
-            className="hero-bg scale-out size-full object-cover scale-125 object-[center_15%]"
+            className="hero-bg scale-out scale-150 size-full object-cover object-[center_15%]"
             alt="hero-bg"
           />
           <img
             src="/images/hero-text.webp"
-            className="scale-out fade-out absolute top-0 scale-125 size-full object-cover object-[center_15%]"
+            className="scale-out scale-150 fade-out absolute top-0 size-full object-cover object-[center_15%]"
             alt="hero-text"
           />
           <button className="fade-out size-17.5 md:size-[7dvw] flex items-center justify-center rounded-full bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 hover:scale-105 transition-transform duration-300">
@@ -107,17 +133,17 @@ export const Hero = () => {
           </button>
           <img
             src="/images/watch-trailer.png"
-            className="scale-out fade-out absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-[15%] w-50 md:w-[13dvw]"
+            className="scale-out scale-150 fade-out absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-[15%] w-50 md:w-[13dvw]"
             alt="watch-trailer"
           />
         </div>
-        <div className="entrance-message invisible absolute w-full top-0 left-0 translate-y-[29%] flex flex-col items-center justify-center gap-5">
+        <div className="entrance-message invisible absolute w-full h-full pt-[0%] top-0 left-0 flex flex-col items-center justify-center gap-5">
           <img
             src="/images/logo.webp"
-            className="max-[769px]:w-[50dvw] w-[13dvw]"
+            className="max-[769px]:w-[50dvw] w-[13dvw] absolute top-0 left-1/2 -translate-x-1/2 translate-y-[90%] sm:translate-y-[45%] lg:translate-y-[90%]"
             alt="logo"
           />
-          <div className="entrance-info scale-125 flex flex-col items-center justify-center gap-5">
+          <div className="entrance-info scale-125 mt-16 md:mt-30 flex flex-col items-center justify-center gap-5">
             <p className="text-[3rem] md:text-[5rem] font-round-bold font-black uppercase leading-18 text-center bg-linear-to-r from-pink-400 via-red-400 to-orange-400 bg-clip-text text-transparent">
               Coming <br /> November 19 <br /> 2026
             </p>
