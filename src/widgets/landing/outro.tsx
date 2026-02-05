cffe0ca2cc79c8311cc0a147517f3eec87276f6d
RockyAtoyan <111198613+RockyAtoyan@users.noreply.github.com>
2026-02-05 15:00:20 +0300

    fix: outro apperance

--- src/widgets/landing/outro.tsx
+++ src/widgets/landing/outro.tsx
@@ -1,5 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 export const Outro = () => {
   useGSAP(() => {
@@ -32,6 +33,8 @@ export const Outro = () => {
         },
         "<"
       );
+
+    ScrollTrigger.refresh();
   });
 
   return (
